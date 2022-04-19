e4f463f0fe9f55fbc38b0f907f4865497b035503
PAINZOR <painzor@outlook.it>
2022-04-19 20:27:33 +0200

    feat=updated

---
+++ Esercizi JS/10 - Blocchi condizionali e cicli/exercise-7.js
@@ -0,0 +1,13 @@
+function sumUntil(maxValue) {
+
+  let numbers = [1,2,3,4,5];
+
+ let result = 0;
+
+ for(let i = 0; i<numbers.length; i++) {
+   result += numbers[i]
+ }
+
+}
+
+console.log(sumUntil(5));
