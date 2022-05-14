e81877c17ddf8d650019551007b5f67dcff3c754
PAINZOR <painzor@outlook.it>
2022-05-14 18:32:26 +0200

    feat: updated

--- Esercizi JS/19 - Object copying/exercise-1.js
+++ Esercizi JS/19 - Object copying/exercise-1.js
@@ -4,12 +4,22 @@ const person1 = {
   age: 25
 };
 
- const person2 = Object.assign({}, person1);
- person1.firstName = "Simon";
+const person2 = person1;
 
-
+person2.firstName = 'Simon';
 
 // Modifica la proprietà "firstName" di person2 in "Simon"
+
+/*
+  Il parametro di person1 viene modificato oltre a quello di person2 poichè alla riga 7
+  non accade altro che assegnare a person2 gli stessi valori di person1
+
+  Nella riga 17-18, avviene il cambio di firstName soltanto in person2
+
+  const person2 = Object.assign({}, person1);
+ person2.firstName = "Simon";
+*/
+
 
 console.log(person1);
 console.log(person2);
