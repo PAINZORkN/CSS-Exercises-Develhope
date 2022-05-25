5acd5207d3fe39cdf5a81e41da1c2f0e198f56b4
PAINZOR <painzor@outlook.it>
2022-05-26 00:46:43 +0200

    feat: Components06 has been completed by checking this commit

--- src/Components/Components02.js
+++ src/Components/Components02.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class SayHello extends Component {
   render() {
     return (
+      <>
       <h1>Hello World </h1>
+      <p>What a beautiful day!</p>
+      </>
     )
   }
 }
