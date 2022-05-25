0620ad0f91763b6dd43e0305fe9aeaf5c6bb6453
PAINZOR <painzor@outlook.it>
2022-05-26 00:37:09 +0200

    feat: added components

---
+++ src/Components/Components02.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SayHello extends Component {
+  render() {
+    return (
+      <h1>Hello World </h1>
+    )
+  }
+}
+
+export default SayHello
---
+++ src/Components/Components03.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import SayHello from "./exercise-7"
+
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <SayHello/>
+            </div>
+        )
+    }
+}
+
+export default App
