856dcfe7bc51da71cdfbf88c69a28c1bd05ddf04
PAINZOR <painzor@outlook.it>
2022-05-25 23:58:57 +0200

    feat:added jsx exercises

---
+++ src/Components/exercise-2.js
@@ -0,0 +1,7 @@
+function Name(value){
+    return(
+        <h1>Hello {value.name}</h1>
+    )
+}
+
+export default Name;
---
+++ src/Components/exercise-5.js
@@ -0,0 +1,8 @@
+function Sum (){
+    const sum = (a,b) => a + b
+    return(
+    <h2>La somma è {sum(5,5)}</h2>
+    )
+}
+
+export default Sum
