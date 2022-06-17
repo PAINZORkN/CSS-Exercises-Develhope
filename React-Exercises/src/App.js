08806b7346b449f4bed37643d0baa593514e634a
PAINZOR <painzor@outlook.it>
2022-06-17 14:34:54 +0200

    feat: pushing event exercises

--- React-Exercises/src/App.js
+++ React-Exercises/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ClickCounter from "./Components/Events01";
+import ClickTracker from "./Components/Events03";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ClickCounter />
+    <ClickTracker />
+    </>
   );
 }
 
