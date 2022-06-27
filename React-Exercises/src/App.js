bafb3954927183b11def9ad9c365e14ab369f48d
PAINZOR <painzor@outlook.it>
2022-06-27 23:35:18 +0200

    feat both exercises completed

--- React-Exercises/src/App.js
+++ React-Exercises/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import { ClickCounter } from './Components/UseState01';
+import { Login } from './Components/UseState02';
+
 
 function App() {
   return (
     <div >
       <ClickCounter />
+      <Login />
     </div>
   );
 }
--- React-Exercises/src/Components/UseState02.jsx
+++ React-Exercises/src/Components/UseState02.jsx
@@ -1,3 +1,76 @@
+import React, { useState } from "react"
+
+
+
+export function Login() {
+
+    const [data, setData] = useState({
+        username: "",
+        password: "",
+        remember: false,
+    });
+
+    const handleValue = (e) => {
+        const { name, value, checked, type } = e.target
+
+        setData((
+            data => {
+                return {
+                    ...data,
+                    [name]: type === "checkbox" ? checked : value
+                }
+            }
+        ))
+    }
+
+    const onLogin = (e) => {
+        e.preventDefault();
+        console.log(data);
+    };
+
+
+
+
+    return (
+        <div>
+            <h1> My Form03</h1>
+            <input
+                name="username"
+                type="text"
+                value={data.username}
+                onChange={handleValue}
+            />
+            <input
+                name="password"
+                type="password"
+                value={data.password}
+                onChange={handleValue}
+            />
+            <input
+                name="remember"
+                type="checkbox"
+                checked={data.remember}
+                onChange={handleValue}
+            />
+
+            <button
+                name="button"
+                type="button"
+                onClick={onLogin}
+                disabled={
+                    data.username === "" || data.password === "" ? true : false
+                }
+            >
+                Click me!
+            </button>
+        </div>
+    )
+
+
+}
+
+
+
 
 
 /*
